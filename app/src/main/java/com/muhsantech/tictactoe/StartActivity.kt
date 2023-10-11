19cf8a104832808cef8dbc950b808ed2c1cdc36e
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-11 09:32:32 +0500

    Start Activity Completed

---
+++ app/src/main/java/com/muhsantech/tictactoe/StartActivity.kt
@@ -0,0 +1,22 @@
+package com.muhsantech.tictactoe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhsantech.tictactoe.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.startGameButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, StartActivity::class.java)
+            startActivity(intent)
+            //finish()
+        }
+    }
+}
