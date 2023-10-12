47d19fd76ab119b7b23c188e855dc7995ad64c56
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-12 10:56:42 +0500

    update setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.muhsantech.tictactoe"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 3332
         versionCode = 1
         versionName = "1.0"
 
